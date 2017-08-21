3d4692341152d0fed9d64cd6a1f9205b41a9af06
mahendraleo42@gmail.com <iamthebest@77>
2017-08-21 16:57:32 +0530

    second commit from android studio

--- app/src/main/java/com/example/leometric/maptest/MapsActivity.java
+++ app/src/main/java/com/example/leometric/maptest/MapsActivity.java
@@ -68,7 +68,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         btn_push_data = findViewById(R.id.btn_push_data);
         btn_push_read = findViewById(R.id.btn_push_read);
 
-
+//
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
